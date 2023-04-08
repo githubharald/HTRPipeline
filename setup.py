e30d6351616f41433a113f9789416b3b46f02490
terramaxiver <119581424+terramaxiver@users.noreply.github.com>
2023-04-08 05:57:34 -0700

    edit setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='htr-pipeline',
     version='1.0.0',
-    description='Handwritten text recognition pipeline, 02/27/2023 code rework.',
+    description='Handwritten text recognition pipeline.',
     author='Harald Scheidl',
     packages=find_namespace_packages(),
     url="https://github.com/githubharald/HTRPipeline",
@@ -12,7 +12,6 @@ setup(
                       'opencv-python',
                       'scikit-learn',
                       'editdistance',
-                      'matplotlib',
                       'path'],
     python_requires='>=3.8',
     package_data={'htr_pipeline.reader.stored_model': ['*']}
